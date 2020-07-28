05abafa6caa44e149f7391ff004770615ff4d778
dgwatts <68703249+dgwatts@users.noreply.github.com>
2020-07-28 10:00:01 +0100

    Add Mongo

--- backend/src/main/java/com/github/dgwatts/interest/InterestRepo.java
+++ backend/src/main/java/com/github/dgwatts/interest/InterestRepo.java
@@ -4,8 +4,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InterestRepo extends MongoRepository<InterestDetails, Long> {
-
-
-
-}
+public interface InterestRepo extends MongoRepository<InterestDetails, String> { }
